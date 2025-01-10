6b7b02500d0835c745bcaf3f0d8b25251b988b17
Adriel Maximus <adrvlogs885@gmail.com>
2025-01-10 19:23:46 -0300

    final commit

--- app.js
+++ app.js
@@ -1,49 +1,52 @@
-import express from 'express';
-import routes from './routes/products';
-import morgan from 'morgan';
-
+const express = require('express');
 const app = express();
-const routeproducts = require('./routes/products');
-const routepedidos = require('./routes/pedidos');
-const BodyParser = require('body-parser');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
+const productRoute = require('./routes/product-route');
+const categoryRoute = require('./routes/category-route');
+const orderRoute = require('./routes/order-route');
+const userRoute = require('./routes/user-route');
+const imageRoute  = require('./routes/image-route');
 
-
-app.use('/pedidos', routepedidos);
-app.use('/products', routeproducts);
 app.use(morgan('dev'));
-app.use(BodyParser.urlencoded({ extended: false }));
-app.use(BodyParser.json());
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: false }));  // apenas dados simples
+app.use(bodyParser.json()); // json de entrada no body
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
+        return res.status(200).send({});
     }
     next();
 });
 
+app.use('/products', productRoute);
+app.use('/categories', categoryRoute);
+app.use('/orders', orderRoute);
+app.use('/users', userRoute);
+app.use('/images', imageRoute);
+
 app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
-});
-app.use('/products', routes);
-app.use('/test', (req, res, next) => {
-    res.status(200).send({
-        
-        message: 'Hello World'
-    });
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
 });
 
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
+    return res.send({
+        erro: {
+            mensagem: error.message
         }
     });
 });
 
-export default app;
+module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
+const app = require('./app');
 const port = process.env.PORT || 3000;
-const app = require('./app').default;
 const server = http.createServer(app);
 server.listen(port);
